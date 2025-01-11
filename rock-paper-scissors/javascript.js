e21f81d0c42bae4552acea43cda9ae76dd2d104a
pravi-x <1998.xar@gmail.com>
2025-01-11 15:09:22 +0200

    Initial commit

---
+++ rock-paper-scissors/javascript.js
@@ -0,0 +1,22 @@
+
+const choices = ["rock", "paper", "scissors"];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+  
+function getComputerChoice() {
+    const choice = getRandomInt(3);  
+    return choices[choice];
+}
+
+function getHumanChoice(){
+
+}
+
+function playRound(humanChoice, computerChoice){
+    
+}
+
+humanScore = 0
+computerScore = 0
